10eb10217d6fbe9bd23405d58ffc7fbbf005e66d
JavaKaran <sharmakaran12092001@gmail.com>
2021-06-21 18:32:17 +0530

    first commit

---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.2 < 0.8.6;
+
+import "./Token.sol";
+
+contract EthSwap {
+  string public name = "EthSwap Instant Exchange";
+  Token public token;
+  uint public rate = 100;
+
+  event TokensPurchased(
+    address account,
+    address token,
+    uint amount,
+    uint rate
+  );
+
+  event TokensSold(
+    address account,
+    address token,
+    uint amount,
+    uint rate
+  );
+
+  constructor(Token _token) public {
+    token = _token;
+  }
+
+  function buyTokens() public payable {
+    // Calculate the number of tokens to buy
+    uint tokenAmount = msg.value * rate;
+
+    // Require that EthSwap has enough tokens
+    require(token.balanceOf(address(this)) >= tokenAmount);
+
+    // Transfer tokens to the user
+    token.transfer(msg.sender, tokenAmount);
+
+    // Emit an event
+    emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+  }
+
+  function sellTokens(address payable admin,uint _amount) public {
+    // User can't sell more tokens than they have
+     require(msg.sender == admin);
+
+    require(token.balanceOf(admin) >= _amount);
+
+    // Calculate the amount of Ether to redeem
+    uint etherAmount = _amount / rate;
+
+    // Require that EthSwap has enough Ether
+    require(address(this).balance >= etherAmount);
+
+    // Perform sale
+    token.transferFrom(admin, address(this), _amount);
+    admin.transfer(etherAmount);
+
+    // Emit an event
+    emit TokensSold(admin, address(token), _amount, rate);
+  }
+
+}
