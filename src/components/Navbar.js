b0bb9ac6e4b220ae80aaa0c1abbc140860a535a4
JavaKaran <sharmakaran12092001@gmail.com>
2021-06-23 12:45:09 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ class Navbar extends Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          EthSwap
+          CryptoExchange
         </a>
 
         <ul className="navbar-nav px-3">
